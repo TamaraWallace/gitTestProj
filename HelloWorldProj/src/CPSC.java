415dd089897bd705624184fe10e8efbf9c4a5bcd
tamara.wallace <tamara.wallace@zone36-wb.cs.ucalgary.ca>
2020-02-03 13:27:25 -0700

    Testing First Commit

---
+++ HelloWorldProj/src/CPSC.java
@@ -0,0 +1,8 @@
+public class CPSC
+{
+        public static void main(String[]  args)
+        {
+                System.out.println("Hello, World");
+                System.out.println("CPSC 233");
+        }
+}
